826a01d27274c0334e411d2a31f6f364557b904b
Deirdre Hegarty <deirdrehegarty@Deirdre-Hegartys-MacBook-Pro.local>
2016-07-13 11:34:37 +0100

    initial commit

---
+++ thank-you.js
@@ -0,0 +1,19 @@
+/*********** function for dropdown in blue container *************/
+$(document).ready(function(){
+
+	$("#dropDown2").hide();
+	$("#dropIcon-row").hide();
+
+    $("#bottomFill2").click(function(){
+    	$("#icon_row2").slideToggle("slow");
+        $("#dropDown2").slideToggle("slow");
+        $("#dropIcon-row").slideToggle("slow", function(){
+			  if($(this).is(":visible")){
+			 	$("#bottomFill2").css("border-radius","0px");
+			 }else{
+			 	$("#bottomFill2").css("border-radius","0px 0px 4px 4px");
+			 }
+        });
+        
+    });
+});
